e9591fd6d70ef83539c0320dabc306f0e2abaf3b
dev-syn <103451081+dev-syn@users.noreply.github.com>
2022-10-13 18:45:15 -0400

    Add FOCUS_CHAT_KEY to config

--- lib/ChatConfig.lua
+++ lib/ChatConfig.lua
@@ -6,16 +6,23 @@
 --[=[
     @prop MAX_MESSAGES number
     @within ChatConfig
-    This configu' is for the max amount of messages that is preserved before being removed the default is 30.
+    This configu' is for the max amount of messages that is preserved before being removed, the default is 30.
 ]=]
 
 --[=[
     @prop MAX_CHAR number
     @within ChatConfig
-    This configu' is for the max amount of characters allowed in a message the default is 100 chars.
+    This configu' is for the max amount of characters allowed in a message, the default is 100 chars.
+]=]
+
+--[=[
+    @prop FOCUS_CHAT_KEY Enum.KeyCode
+    @within ChatConfig
+    This configu' is for focusing/unfocusing the chat text field, the default is Enum.KeyCode.Slash
 ]=]
 
 return {
     MAX_MESSAGES = 30, -- The max amount of messages that is stored before being removed
-    MAX_CHAR = 100 -- The max amount of chars in a message
+    MAX_CHAR = 100, -- The max amount of chars in a message
+    FOCUS_CHAT_KEY = Enum.KeyCode.Slash
 };
