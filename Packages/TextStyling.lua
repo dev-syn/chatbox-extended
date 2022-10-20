6bef4a0cb7ade51515e2dce753fdb39da080a07b
dev-syn <103451081+dev-syn@users.noreply.github.com>
2022-10-20 18:07:43 -0400

    Add TextStyling package

--- Packages/TextStyling.lua
+++ Packages/TextStyling.lua
@@ -1,1 +1,3 @@
-return require(script.Parent._Index["dev-syn_text-styling@0.1.2"]["text-styling"])
+local REQUIRED_MODULE = require(script.Parent._Index["dev-syn_text-styling@0.1.2"]["text-styling"])
+export type TextStyling = REQUIRED_MODULE.TextStyling 
+return REQUIRED_MODULE
