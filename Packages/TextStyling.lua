4507a43a5afa58e5a3a858916e78faac8707ac42
dev-syn <103451081+dev-syn@users.noreply.github.com>
2022-10-20 18:16:07 -0400

    Add TextStyling type in wally package module

--- Packages/TextStyling.lua
+++ Packages/TextStyling.lua
@@ -1,3 +1,3 @@
-local REQUIRED_MODULE = require(script.Parent._Index["dev-syn_text-styling@0.1.2"]["text-styling"])
+local REQUIRED_MODULE = require(script.Parent._Index["dev-syn_text-styling@0.1.3"]["text-styling"])
 export type TextStyling = REQUIRED_MODULE.TextStyling 
 return REQUIRED_MODULE
