7816d3cca51b92bd9764bc88510d054a31efabe4
dev-syn <103451081+dev-syn@users.noreply.github.com>
2022-10-21 06:42:53 -0400

    Add TextStyling & Permissions package

--- ServerPackages/Permissions.lua
+++ ServerPackages/Permissions.lua
@@ -1,1 +1,5 @@
-return require(script.Parent._Index["dev-syn_permissions@0.2.1"]["permissions"])
+local REQUIRED_MODULE = require(script.Parent._Index["dev-syn_permissions@0.3.1"]["permissions"])
+export type Schema_Group = REQUIRED_MODULE.Schema_Group 
+export type Group = REQUIRED_MODULE.Group 
+export type Permissions = REQUIRED_MODULE.Permissions
+return REQUIRED_MODULE
